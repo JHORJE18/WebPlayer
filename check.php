5fb71e6140db0b1001dfb3a3a44a07f8c92fda8d
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-05-24 17:04:31 +0200

    Corrección errores

--- check.php
+++ check.php
@@ -4,8 +4,8 @@ $dbuser="root";
 $dbpass="master";
 $dbname="coffee_sound";
 $chandle = mysql_connect("localhost", $dbuser, $dbpass) or die("Error conectando a la BBDD");
-echo "Conectado correctamente
-";
+echo "Conectado correctamente";
+
 mysql_select_db($dbname, $chandle) or die ($dbname . " Base de datos no encontrada." . $dbuser);
 echo "Base de datos " . $database . " seleccionada";
 mysql_close($chandle);
