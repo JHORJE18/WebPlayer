7e85e28e20a51d2d0516e6d372be21afc14be586
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-05-24 16:57:14 +0200

    Muestra error al conectar

--- index.php
+++ index.php
@@ -22,10 +22,13 @@
 
 		  $sql = "SELECT * FROM `cancion`";
 		  echo $sql;
-		  if ($resultado = $conexion->query($sql)){
+			$result= $conexion -> query($sql);
+		  if ($result){
 			  echo 'Se ha consultado <br>';
 			  $fila = $resultado->fetch_array();
 			  echo $fila[0];
+		  } else {
+			  echo 'Error al conectar loco';
 		  }
 
 		  //Descargar cancion
